76eaecf788dd18a4b33ea46dbe47f184c84b9986
Seb Julliand <sjulliand@arcadsoftware.com>
2023-12-19 16:01:48 +0100

    Working version with start/stop support
    Signed-off-by: Seb Julliand <sjulliand@arcadsoftware.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,47 @@
 import * as vscode from 'vscode';
-import { initializeAFSBrowser } from './afsBrowser';
+import { Code4i } from './code4i';
+import { AFSServerWrappers } from './types';
+import { initializeAFSBrowser } from './views/afsBrowser';
 
-export function activate(context: vscode.ExtensionContext) {	
+
+export async function activate(context: vscode.ExtensionContext) {
+	await Code4i.initialize();
 	initializeAFSBrowser(context);
-	console.log(vscode.l10n.t('The extension "arcad-afs-for-ibm-i" is now active!'));
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+
+}
+
+export namespace Configuration {
+	function getAllWrappers() {
+		return (vscode.workspace.getConfiguration("arcad-afs").get<AFSServerWrappers[]>("wrappers") || []);
+	}
+
+	export async function getWrappers() {
+		const host = Code4i.getConnection().currentHost;
+		const wrappers = getAllWrappers();
+		let wrappersForHost = wrappers.find(w => w.host === host);
+		if (!wrappersForHost) {
+			wrappersForHost = {
+				host,
+				locations: []
+			};
+			await updateWrappers(wrappersForHost);
+		}
+		return wrappersForHost;
+	}
+
+	export async function updateWrappers(wrappers: AFSServerWrappers) {
+		const allWrappers = getAllWrappers();
+		let currentConfig = allWrappers.find(w => w.host === wrappers.host);
+		if (currentConfig) {
+			currentConfig.locations = wrappers.locations;
+		}
+		else {
+			allWrappers.push(wrappers);
+		}
+
+		await vscode.workspace.getConfiguration("arcad-afs").update("wrappers", allWrappers, vscode.ConfigurationTarget.Global);
+	}
+}
