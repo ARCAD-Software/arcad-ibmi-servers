a1b04a77e5494ab9424bb02b7fe098a9b42ff8e3
Seb Julliand <sjulliand@arcadsoftware.com>
2024-01-30 14:53:55 +0100

    Added access to online help from view and palette
    Signed-off-by: Seb Julliand <sjulliand@arcadsoftware.com>

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,9 @@ import { initializeAFSBrowser } from './views/serversBrowser';
 export async function activate(context: vscode.ExtensionContext) {
 	await Code4i.initialize();
 	initializeAFSBrowser(context);
+	context.subscriptions.push(
+		vscode.commands.registerCommand("arcad-afs-for-ibm-i.open.online.help", () => vscode.commands.executeCommand("vscode.open","https://arcad-software.github.io/arcad-ibmi-servers"))
+	);
 }
 
 export function deactivate() {
