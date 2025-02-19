d37718567063e22707ea0d30f2edfbb922890ae6
Seb Julliand <sjulliand@arcadsoftware.com>
2025-02-19 10:25:22 +0100

    Add user setting with option to restart servers after update
    Signed-off-by: Seb Julliand <sjulliand@arcadsoftware.com>

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	await Code4i.initialize(context);
 	initializeAFSBrowser(context);
 	context.subscriptions.push(
-		vscode.commands.registerCommand("arcad-afs-for-ibm-i.open.online.help", () => vscode.commands.executeCommand("vscode.open","https://arcad-software.github.io/arcad-ibmi-servers"))
+		vscode.commands.registerCommand("arcad-afs-for-ibm-i.open.online.help", () => vscode.commands.executeCommand("vscode.open", "https://arcad-software.github.io/arcad-ibmi-servers"))
 	);
 }
 
